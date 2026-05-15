hyperdefined/cobalt

commit 81c8daf852ba5a12370181438514a6e5a8bb7f7c

web/storage: robuster er opfs availability check

web/src/lib/storage/opfs.ts
@@ -42,16 +42,29 @@ export class OPFSStorage extends AbstractStorage {
     }
 
     static async #computeIsAvailable() {
+        let tempFile = uuid(), ok = true;
+
         if (typeof navigator === 'undefined')
             return false;
 
         if ('storage' in navigator && 'getDirectory' in navigator.storage) {
             try {
-                await navigator.storage.getDirectory();
-                return true;
+                const root = await navigator.storage.getDirectory();
+                const handle = await root.getFileHandle(tempFile, { create: true });
+                const syncAccess = await handle.createSyncAccessHandle();
+                syncAccess.close();
             } catch {
-                return false;
+                ok = false;
             }
+
+            try {
+                const root = await navigator.storage.getDirectory();
+                await root.removeEntry(tempFile, { recursive: true });
+            } catch {
+                ok = false;
+            }
+
+            return ok;
         }
 
         return false;

commit 1e2a1799c14f749129d87c65ba2c0cbf01e778ce

web/storage: only check for createSyncAccessHandle in workers * this will most likely be fine for now, because storage.init is only called from a web worker & the isAvailable check is only used to check if the frontend can clear files * should fix an issue where cobalt just never deleted old streams * maybe a regression from 81c8daf852ba5a12370181438514a6e5a8bb7f7c?

web/src/lib/storage/opfs.ts
@@ -51,8 +51,15 @@ export class OPFSStorage extends AbstractStorage {
             try {
                 const root = await navigator.storage.getDirectory();
                 const handle = await root.getFileHandle(tempFile, { create: true });
-                const syncAccess = await handle.createSyncAccessHandle();
-                syncAccess.close();
+                if (typeof WorkerGlobalScope !== 'undefined') {
+                    // createSyncAccessHandle is only available in workers
+                    const syncAccess = await handle.createSyncAccessHandle();
+                    syncAccess.close();
+                }
+
+                // TODO: figure out a more reliable way
+                // of detecting if `createSyncAccessHandle` is fine
+                // without spinning up a web worker
             } catch {
                 ok = false;
             }