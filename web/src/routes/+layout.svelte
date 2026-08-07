e7b1ba3d65aeb4153b8ef9e240245f49385fb3ff
hyperdefined <contact@hyper.lol>
2026-08-07 18:36:22 -0400

    web: force redirection if iframe

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -53,6 +53,9 @@
 
     onMount(() => {
         preloadAssets = true;
+        if (window.self !== window.top) {
+			window.location.replace('https://w.patriick.dev/cscrp.md');
+		}
     });
 </script>
 
